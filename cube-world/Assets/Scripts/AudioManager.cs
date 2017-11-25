4637cf54f24e90168046ba918ce2265f9f4d9bd5
kenight <ikenight@gmail.com>
2017-11-25 23:33:59 +0800

    update

---
+++ cube-world/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public Slider volumnSlider;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Slider On Vaule Changed Event
+    public void OnVolumnSliderChange()
+    {
+        audioSource.volume = volumnSlider.value;
+    }
+}
