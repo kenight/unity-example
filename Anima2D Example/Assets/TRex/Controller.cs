ba4e97c047d981f8d00fbce24cc33c64fd7c848f
kenight <ikenight@gmail.com>
2017-12-05 23:40:22 +0800

    update

---
+++ Anima2D Example/Assets/TRex/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float xAixs = Input.GetAxis("Horizontal");
+
+		Vector3 eulerAngles = transform.localEulerAngles;
+
+		if (xAixs < 0)
+			eulerAngles.y = 180;
+		else if (xAixs > 0)
+			eulerAngles.y = 0;
+
+		transform.localRotation = Quaternion.Euler(eulerAngles);
+
+		animator.SetFloat("Speed", Mathf.Abs(xAixs));
+	}
+}
