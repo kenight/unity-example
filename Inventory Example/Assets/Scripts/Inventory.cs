7b3c8e3266c0f8f2fd851d6d2b4b10eb41f173b7
kenight <ikenight@gmail.com>
2017-12-13 23:29:26 +0800

    update

---
+++ Inventory Example/Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+	public GameObject slotPrefab;
+	public GameObject slotPanel;
+
+	private List<GameObject> slots = new List<GameObject>();
+
+	void Start() {
+		InitInventory();
+	}
+
+	void Update() {
+
+	}
+
+	void InitInventory() {
+		for (int i = 0; i < 20; i++) {
+			slots.Add(Instantiate(slotPrefab));
+			slots[i].transform.SetParent(slotPanel.transform);
+		}
+	}
+
+	public void AddItemToInventory() {
+
+	}
+}
