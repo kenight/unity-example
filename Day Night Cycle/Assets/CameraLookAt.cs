aa7ac0b067e476fefdd654878ee5d0e3bea07e02
kenight <ikenight@gmail.com>
2017-12-17 22:59:50 +0800

    update

---
+++ Day Night Cycle/Assets/CameraLookAt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour {
+
+	public Transform target;
+
+	void LateUpdate() {
+		transform.LookAt(new Vector3(target.position.x, 0, target.position.z));
+	}
+}
---
+++ Day Night Cycle/Assets/DayNightCycle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+	void Update() {
+		transform.RotateAround(Vector3.zero, Vector3.right, 10 * Time.deltaTime);
+		transform.LookAt(Vector3.zero);
+	}
+}
