9e21396c6635674acab018cef0b28b0edb7a5a41
MaiYo <ikenight@gmail.com>
2017-12-31 15:54:06 +0800

    update

--- UI Example/Assets/Scripts/Network/ArenaGameManager.cs
+++ UI Example/Assets/Scripts/Network/ArenaGameManager.cs
@@ -8,12 +8,17 @@ public class ArenaGameManager : Photon.PunBehaviour {
 
 	// 通过 Photon Instantiate 的 prefab 必须放在 Resources 文件夹中
 	public GameObject playerPrefab;
+	private Transform spawnPoint;
 
 	void Start() {
+		SpawnPlayer();
+	}
+
+	void SpawnPlayer() {
 		// Instantiate Player
 		if (playerPrefab) {
 			// 注意第一个参数是 playerPrefab 的 name
-			PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(-3.5f, 0.8f, 0), Quaternion.identity, 0);
+			PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, Quaternion.identity, 0);
 		}
 	}
 
---
+++ UI Example/Assets/Scripts/Network/SpawnPointManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointManager : Photon.PunBehaviour, IPunObservable {
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) { } else { }
+	}
+}
