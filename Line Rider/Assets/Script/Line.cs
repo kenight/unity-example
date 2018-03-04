99b0efc4a7c36275a97580572fd529b3855c1b3e
kenight <ikenight@gmail.com>
2018-03-05 00:19:36 +0800

    update

---
+++ Line Rider/Assets/Script/Line.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+	public LineRenderer lineRenderer;
+	public EdgeCollider2D edgeCol;
+	List<Vector2> points;
+
+	// 画线的逻辑, 原理就是动态设置 lineRenderer
+	public void DrawLine(Vector2 newPos) {
+		// 第一个点
+		if (points == null) {
+			points = new List<Vector2>();
+			AddPoint(newPos);
+			return;
+		}
+
+		// 超过一定的移动范围后，才增加新的点
+		if (Vector2.Distance(points.Last(), newPos) > 0.3f) { // using System.Linq
+			AddPoint(newPos);
+		}
+	}
+
+	void AddPoint(Vector2 newPos) {
+		points.Add(newPos);
+		// 首先需要重置 lineRenderer position element 的数量
+		lineRenderer.positionCount = points.Count;
+		// 设置新增 element 的位置
+		lineRenderer.SetPosition(points.Count - 1, newPos);
+		// 重置 EdgeCollider2D 的坐标点数据
+		if (points.Count > 1)
+			edgeCol.points = points.ToArray();
+	}
+}
---
+++ Line Rider/Assets/Script/LineCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour {
+
+	public GameObject line;
+	Line current;
+
+	void Update() {
+		// 左键按下：创建 Line gameobject
+		if (Input.GetMouseButtonDown(0)) {
+			GameObject lineGO = Instantiate(line);
+			current = lineGO.GetComponent<Line>();
+		}
+
+		// 鼠标移动：画线
+		if (current != null) {
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			current.DrawLine(mousePos);
+		}
+
+		// 左键释放：停止画线
+		if (Input.GetMouseButtonUp(0)) {
+			current = null;
+		}
+	}
+}
