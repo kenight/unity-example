fd164207410bca61d170cc0567a5a6ef5eb9fc92
MaiYo <ikenight@gmail.com>
2018-03-06 19:17:34 +0800

    update

---
+++ Line Rider/Assets/Script/GlowLineRider/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public float speed = 10f;
+	public float rotatePower = 5f;
+
+	Rigidbody2D rb;
+	bool moving = false, isGround = false;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space))
+			moving = true;
+
+		if (Input.GetKeyUp(KeyCode.Space))
+			moving = false;
+	}
+
+	void FixedUpdate() {
+		if (moving) {
+			if (isGround)
+				rb.AddForce(transform.right * speed);
+			else
+				rb.AddTorque(rotatePower);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		isGround = true;
+	}
+
+	private void OnCollisionExit2D(Collision2D other) {
+		isGround = false;
+	}
+}
