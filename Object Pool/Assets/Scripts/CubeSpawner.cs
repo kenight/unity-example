9664899fbfef617de2254f740f919904320a1e83
kenight <ikenight@gmail.com>
2018-03-27 01:14:53 +0800

    update

---
+++ Object Pool/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+	public ObjectPool cubePool;
+
+	void FixedUpdate() {
+		float xPos = Random.Range(transform.position.x - 1, transform.position.x + 1);
+		float zPos = Random.Range(transform.position.z - 1, transform.position.z + 1);
+		Vector3 pos = new Vector3(xPos, transform.position.y, zPos);
+		GameObject go = cubePool.InstantiateFromPool(pos, Quaternion.identity);
+		go.GetComponent<Cube>().OnInstantiated();
+	}
+}
---
+++ Object Pool/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+
+	public GameObject obj;
+	[Range(1, 1000)]
+	public int size = 10;
+
+	// 使用队列存储游戏对象<先进先出>
+	Queue<GameObject> poolQueue;
+
+	void Start() {
+		poolQueue = new Queue<GameObject>();
+
+		for (int i = 0; i < size; i++) {
+			GameObject _obj = Instantiate(obj);
+			_obj.SetActive(false);
+			// 向 Queue 的末尾添加一个对象
+			poolQueue.Enqueue(_obj);
+		}
+	}
+
+	public GameObject InstantiateFromPool(Vector3 position, Quaternion rotation) {
+		if (poolQueue.Count == 0)
+			return null;
+
+		// 移除并返回在 Queue 的开头的对象
+		GameObject _obj = poolQueue.Dequeue();
+		_obj.transform.position = position;
+		_obj.transform.rotation = rotation;
+		_obj.SetActive(true);
+
+		// 重新将 _obj 添加 Queue 的末尾，进行重用
+		poolQueue.Enqueue(_obj);
+		return _obj;
+	}
+
+}
